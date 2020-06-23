26b67db524f45cddd54517a23f107855303b5abf
sliverz6 <52848569+sliverz6@users.noreply.github.com>
2020-06-23 21:15:30 +0900

    slider

--- component2/slider/script.js
+++ component2/slider/script.js
@@ -1,26 +1,40 @@
+class UIHelper {
+    static slide(type) {
+        const slideItems = document.querySelectorAll('.slider__item');
+
+        slideItems.forEach(slideItem => {
+            const containerWidth = slideItem.parentElement.clientWidth;
+            const currentXPos = parseInt(slideItem.style.left);
+            const targetXPos = type === 'left' ? currentXPos + containerWidth : currentXPos - containerWidth;
+            slideItem.style.left = `${targetXPos}px`;
+        });
+    }
+}
+
 class SlideItem {
-    constructor(element) {
+    constructor(index, element) {
+        this.index = index;
         this.element = element;
     }
 }
 
 class Slider {
     slideList = [];
+    currentSlideIdx;
 
     init() {
-        this.connectButtonElement();
+        this.connectButtonsElement();
         this.connectSlideElement();
     }
 
     slide(type) {
-        const slideItems = document.querySelectorAll('.slider__item');
-
-        slideItems.forEach((slideItem, index) => {
-            const containerWidth = slideItem.parentElement.clientWidth;
-            const currentXPos = parseInt(slideItem.style.left);
-            const targetXPos = type === 'left' ? currentXPos + containerWidth : currentXPos - containerWidth;
-            slideItem.style.left = `${targetXPos}px`;
-        });
+        if (this.currentSlideIdx > 0 && type === 'left') {
+            this.currentSlideIdx--;
+            UIHelper.slide(type);
+        } else if (this.currentSlideIdx < this.slideList.length - 1 && type === 'right') {
+            this.currentSlideIdx++;
+            UIHelper.slide(type);
+        }
     }
 
     buttonHandler(e) {
@@ -31,7 +45,7 @@ class Slider {
         }
     }
 
-    connectButtonElement() {
+    connectButtonsElement() {
         const buttons = document.querySelectorAll('.slider__btn');
         for (const button of buttons) {
             button.addEventListener('click', this.buttonHandler.bind(this));
@@ -40,13 +54,25 @@ class Slider {
 
     connectSlideElement() {
         const slideItems = document.querySelectorAll('.slider__item');
+        let defaultSlideIdx;
+        slideItems.forEach((slideItem, index) => {
+            slideItem.style.position = 'absolute';
+            this.slideList.push(new SlideItem(index, slideItem));
+            if (slideItem.classList.contains('home')) {
+                defaultSlideIdx = index;
+            }
+        });
+
+        this.setDefaultView(defaultSlideIdx);
+    }
+
+    setDefaultView(slideIdx = 0) {
+        const slideItems = document.querySelectorAll('.slider__item');
         slideItems.forEach((slideItem, index) => {
             const containerWidth = slideItem.parentElement.clientWidth;
-            slideItem.style.position = 'absolute';
-            slideItem.style.left = `${containerWidth * index}px`;
-
-            this.slideList.push(new SlideItem(slideItem));
+            slideItem.style.left = `${(containerWidth * index) - (containerWidth * slideIdx)}px`;
         });
+        this.currentSlideIdx = slideIdx;
     }
 }
 
